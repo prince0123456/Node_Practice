3ebdb6cf063b4e18b0dc514517f4ffb0ac7e6a73
Prince Gupta <princegupta.pg46@gmail.com>
2022-12-11 13:52:14 +0530

    Express_demo

--- ExpressDemo.js
+++ ExpressDemo.js
@@ -1,8 +1,9 @@
-const { application } = require("express");
+// const { application } = require("express");
 const expr=require("express");
 const aap=expr();
 aap.get('/',(req,res)=>{
- res.send("hello , this is home page")
+    console.log("data sent " ,req.query.name);
+ res.send("hello , this is home page"+" "+req.query.name)
 })
 
 aap.get('/about',(req,res)=>{
