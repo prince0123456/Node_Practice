da4f4693e9ccfb91ba4c3e1eb67cdd393f17df95
Prince Gupta <princegupta.pg46@gmail.com>
2022-12-11 14:02:45 +0530

    HTML_render

--- ExpressDemo.js
+++ ExpressDemo.js
@@ -3,14 +3,31 @@ const expr=require("express");
 const aap=expr();
 aap.get('/',(req,res)=>{
     console.log("data sent " ,req.query.name);
- res.send("hello , this is home page"+" "+req.query.name)
+ res.send("<h1>hello , this is home page</h1>"+" "+req.query.name)
 })
 
 aap.get('/about',(req,res)=>{
-    res.send("hello , this is about page")
+    res.send("hello , this is about page"+`
+    <input type='text' value="${req.query.name}"/>
+    <a href="/about">about</a>
+    <a href="/help">help</a>
+    `)
    })
 
    aap.get('/help',(req,res)=>{
-    res.send("hello , this is help page")
-   })
+    res.send([
+        {
+            name:"prince",
+            email:'axyz@test.com'
+        },
+        {
+            name:'prince',
+            email:'axyz@test.com'
+        },
+        {
+            name:'prince',
+            email:'axyz@test.com'
+        }
+    ]);
+   });
    aap.listen(3001);
