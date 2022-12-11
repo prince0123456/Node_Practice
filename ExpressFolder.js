0945534c20bd4597f05622ed679c3b6e0b97f2a7
Prince Gupta <princegupta.pg46@gmail.com>
2022-12-11 15:20:00 +0530

    template_engine_ejs

--- ExpressFolder.js
+++ ExpressFolder.js
@@ -2,12 +2,23 @@ const express=require('express');
 const path=require('path');
 const app=express();
 const publicpath=path.join(__dirname,'public');
+
+app.set('view engine','ejs')
 // app.use(express.static(publicpath));
 app.get("/about",(req,res)=>{
     res.sendFile(`${publicpath}/about.html`)
 })
 app.get("/home",(req,res)=>{
     res.sendFile(`${publicpath}/Home.html`)
+})
+app.get("/profile",(req,res)=>{
+    const user=
+        {
+            name:'prince',
+            email:'shrk@gmasil.com'
+        }
+    
+    res.render('profile',{user})
 })
 app.get("*",(req,res)=>{
     res.sendFile(`${publicpath}/404.html`)
