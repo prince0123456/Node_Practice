b1f4ff5684b1423f5a0c5ab06b61abe02818b34f
Prince Gupta <princegupta.pg46@gmail.com>
2022-12-12 20:22:08 +0530

    post_Api

---
+++ mongoDB/crud/postApi.js
@@ -0,0 +1,23 @@
+const express=require('express');
+const dbConnect=require('./mongoCon');
+const app=express();
+
+//middle
+ app.use(express.json());
+
+//api
+
+app.get('/',async (req,res)=>{
+    let data= await dbConnect();
+    data= await data.find().toArray();
+    console.log(data)
+     res.send(data);
+})
+app.post('/',async (req,res)=>{
+    console.log(req.body);
+    let data=await dbConnect();
+    let result = await data.insertOne(req.body);
+    console.log(result)
+    res.send('hello post')
+})
+app.listen(3005)
