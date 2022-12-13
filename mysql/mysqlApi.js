28847cb1f64a7ddd5b396e6934f05747ccf0ddba
Prince Gupta <princegupta.pg46@gmail.com>
2022-12-13 22:50:03 +0530

    mysql_getApi

--- mysql/mysqlApi.js
+++ mysql/mysqlApi.js
@@ -1,7 +1,15 @@
 const express=require('express')
+const con=require("./config");
 const app=express();
 app.get("/",(req,res)=>{
-    res.send("rout done");
+    con.query("SELECT * FROM city",(err,result)=>{
+        if(err){
+            res.send(err);
+        }else{
+            res.send(result)
+        }
+    })
+    // res.send("rout done");
 });
 
-app.listen(5000);
+app.listen(3001);
