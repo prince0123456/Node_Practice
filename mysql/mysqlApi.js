6420d400c6e3b30caf0b44ea18380cbcf29f2632
Prince Gupta <princegupta.pg46@gmail.com>
2022-12-14 13:29:00 +0530

    put

--- mysql/mysqlApi.js
+++ mysql/mysqlApi.js
@@ -1,6 +1,8 @@
 const express=require('express')
 const con=require("./config");
 const app=express();
+
+app.use(express.json())
 app.get("/",(req,res)=>{
     con.query("SELECT * FROM city",(err,result)=>{
         if(err){
@@ -12,14 +14,20 @@ app.get("/",(req,res)=>{
     // res.send("rout done");
 });
 app.post("/",(req,res)=>{
-    con.query("SELECT * FROM users",(err,result)=>{
-        if(err){
-            res.send(err);
-        }else{
-            res.send(result)
-        }
+    const data={name:"prince123",lastname:"12delta"};
+    con.query("INSERT INTO users SET ?",data,(err,result,fields)=>{
+        if(err)throw err;
+        res.send(result);
     })
     // res.send("rout done");
 });
+app.put("/:id",(req,res)=>{
+    // res.send("update API")
+    const data=[req.body.name,req.body.lastname,req.params.id]
+    con.query("UPDATE users SET name=?,lastname=? where iduser = ?",data,(err,result,fields)=>{
+          if(err) throw err;
+          res.send(result);
+    })
+})
 
-app.listen(3001);
+app.listen(3006);
